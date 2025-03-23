3c1644bde156d5ea24d28f3c9005ca40851ea52e
chicoumbetov <shyngysmaaan@gmail.com>
2025-03-23 21:30:13 +0100

    catch if 0 questions

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -16,7 +16,9 @@ export async function POST(request: Request) {
         The job experience level is ${level}.
         The tech stack used in the job is: ${techstack}.
         The focus between behavioural and technical questions should lean towards: ${type}.
-        The amount of questions required is: ${amount}.
+        The amount of questions required is: ${
+          amount.toString() === "0" ? 1 : amount
+        }.
         Please return only the questions, without any additional text.
         The questions are going to be read by a voice assistant so do not use "/" or "*" or any other special characters which might break the voice assistant.
         Return the questions formatted like this:
