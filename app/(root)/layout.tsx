65c047feb8c0e0975bc8be091ebbfc5b08c3d084
chicoumbetov <shyngysmaaan@gmail.com>
2025-03-23 14:46:17 +0100

    :recycle: isAuth implement to root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,15 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 import { ReactNode } from "react";
-// import { redirect } from "next/navigation";
 
-// import { isAuthenticated } from "@/lib/actions/auth.action";
+import { isAuthenticated } from "@/lib/actions/auth.action";
 
 const Layout = async ({ children }: { children: ReactNode }) => {
-  /*
   const isUserAuthenticated = await isAuthenticated();
   if (!isUserAuthenticated) redirect("/sign-in");
-  */
 
   return (
     <div className="root-layout">
